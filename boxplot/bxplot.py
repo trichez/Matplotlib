2b24a7a6d73adb39b85a2dd28e905c2e3ff09411
trichez <mh.trichez@gmail.com>
2018-04-23 09:54:11 -0300

    lil changes

--- boxplot/bxplot.py
+++ boxplot/bxplot.py
@@ -12,17 +12,25 @@ collectn_5 = np.random.normal(70, 25, 200)
 #np.random.normal(mean, stdDev, n)
 data_to_plot = [collectn_1, collectn_2, collectn_3, collectn_4]
 
-plt.figure("First 4 data collections")
+
+
+plt.figure("Last data collection")
+plt.boxplot(collectn_5, 0)
+plt.grid(ls='--')
+ax = plt.axes()
+ax.set_xticklabels(['Collection 5'])
+
+
+
+
+
+
+plt.figure("First 4 data collections in notched boxplots")
 plt.boxplot(data_to_plot, 1)
 ax = plt.axes()
 ax.set_xticklabels(['Collection 1', 'Collection 2', 'Collection 3', 'Collection 4'])
 
 
-plt.figure("Last data collection in a notched boxplot")
-plt.boxplot(collectn_5, 0)
-plt.grid(ls='--')
-ax = plt.axes()
-ax.set_xticklabels(['Collection 5'])
 
 
 plt.show()
